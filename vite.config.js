9145bee548f9f32ae2c8fa5d404f24c90af9a119
EliteDevSolution <fstorm707@gmail.com>
2024-07-05 11:18:30 +0900

    Updated: vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      '/': {
         target: 'https://now-trade-api.vercel.app',
         changeOrigin: true
       }
