21921722186a256594444093a2709cd6fed5e75d
EliteDevSolution <fstorm707@gmail.com>
2024-07-05 11:29:32 +0900

    Fix: Issue

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://now-trade-api.vercel.app' });
 
 export const apiSlice = createApi({
   baseQuery,
